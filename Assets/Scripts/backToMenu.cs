406f5e574acc14a2fc309483ba6b2dd99b44c734
lynnecc98 <lynnecc98@gmail.com>
2019-04-26 00:59:53 -0400

    Added working buttons and created about and credits scene. Restructured game scene but need to fix cutscene ratio

---
+++ Assets/Scripts/backToMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class backToMenu : MonoBehaviour
+{
+    public void menuScreen() {
+        SceneManager.LoadScene("Title Screen");
+    }
+}
---
+++ Assets/Scripts/gameStarter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameStarter : MonoBehaviour
+{
+    public void StartGame() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void goToAbout() {
+        SceneManager.LoadScene("About");
+    }
+
+    public void goToCredits()
+    {
+        SceneManager.LoadScene("CreditsMenu");
+    }
+}
